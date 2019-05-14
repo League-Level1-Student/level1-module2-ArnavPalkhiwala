00c33900e71622da4a8fff1b1396aadd6d4cec77
League Student <league@WTS9.attlocal.net>
2019-05-13 17:34:51 -0700

    level1

---
+++ src/Minion.java
@@ -0,0 +1,55 @@
+public class Minion {
+
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
+
+	public Minion(String name, int eyes, String color, String master) {
+
+		this.name = name;
+		this.eyes = eyes;
+		this.color = color;
+		this.master = master;
+
+	}
+
+	String getName() {
+		return this.name;
+	}
+
+	void setName(String name) {
+
+		this.name = name;
+	}
+
+	int getEyes() {
+		return this.eyes;
+	}
+
+	void setEyes(int eyes) {
+
+		this.eyes = eyes;
+	}
+	
+	String getColor() {
+		return this.color;
+	}
+	
+	void setColor(String color) {
+		
+		this.color = color;
+	}
+	
+	String getMaster() {
+		return this.master;
+	}
+	
+	void setMaster(String master) {
+		
+		this.master = master;
+	}
+	
+	
+
+}
